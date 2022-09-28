511594a51be0bac0b6f2173e6bd466442bab1c91
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-09-29 02:01:03 +0300

    Adds files

--- src/day_night.js
+++ src/day_night.js
@@ -3,4 +3,5 @@ document.querySelector('.tumbler-wrapper').onclick = onChangeTheme;
 function onChangeTheme() {
   document.body.classList.toggle('night-mode');
   document.getElementsByClassName('footer')[0].classList.toggle('night-mode');
+  // document.getElementById('pagination').classList.toggle('night-mode');
 }
--- src/renderCard.js
+++ src/renderCard.js
@@ -4,9 +4,9 @@ const BASE_URL = 'https://image.tmdb.org/t/p/w342';
 // рендер карточки фільма та створення фільмотеки
 
 export default function renderFilmCard(films) {
-    const markup = films.map(({id, poster_path, title, genre_ids, release_date}) => {
-
-    return `
+  const markup = films
+    .map(({ id, poster_path, title, genre_ids, release_date }) => {
+      return `
         <li class="film__card" id=${id}>
             <a><img src = "${BASE_URL}${poster_path}" alt="${title}" loading="lazy" /></a>
             <div class="film__info">
@@ -15,8 +15,8 @@ export default function renderFilmCard(films) {
                 <p class="film__relis">${release_date}</p>
             </div>
         </li>
-    `
-    }).join("");
-    gallery.insertAdjacentHTML('beforeend', markup);
+    `;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
-
