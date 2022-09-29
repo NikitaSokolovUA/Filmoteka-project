2b04b1acf956a5da8453ae16039d5d0fa5fbf8e0
Nikita Sokolov UA <nsokolov0309@gmail.com>
2022-09-29 16:26:47 +0300

    add modal on click

---
+++ src/modal.js
@@ -0,0 +1,48 @@
+const refs = {
+    modal: document.querySelector('[data-modal]'),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    listOfFilm: document.querySelector('.film__list')
+}
+
+
+refs.listOfFilm.addEventListener('click', onClickFilm);
+refs.closeModalBtn.addEventListener('click', onCloseModal)
+refs.modal.addEventListener('click', onClickBackdropModalClose)
+
+
+
+function onClickFilm(e) { 
+    const chosenFilm = e.target.parentNode.parentNode
+
+    
+    if (chosenFilm.nodeName !== "LI") {
+        return
+    }
+
+    onOpenModal()
+}
+
+function onOpenModal() {
+    window.addEventListener('keydown', inKeyDownEscModalClose)
+    refs.modal.classList.toggle("backdrop--is-hidden");
+}
+  
+  function onCloseModal() {
+    window.removeEventListener('keydown',inKeyDownEscModalClose)
+    refs.modal.classList.toggle("backdrop--is-hidden");
+}
+  
+ function onClickBackdropModalClose(event) {
+    if (event.target === event.currentTarget) {
+      onCloseModal()
+    }
+}
+  
+  function inKeyDownEscModalClose(event) {
+    const KEY_CODE_ESCAPE = 'Escape';
+
+    if (event.code === KEY_CODE_ESCAPE) {
+      onCloseModal()
+    }
+}
+  
