e0254b6887e308ae3de22dcaa4bdc7740d88fffa
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-10-01 00:36:57 +0300

    adds pagination styles

--- src/js/day_night.js
+++ src/js/day_night.js
@@ -3,4 +3,10 @@ document.querySelector('.tumbler-wrapper').onclick = onChangeTheme;
 function onChangeTheme() {
   document.body.classList.toggle('night-mode');
   document.getElementsByClassName('footer')[0].classList.toggle('night-mode');
+  document
+    .getElementsByClassName('my-pagination')[0]
+    .classList.toggle('night-mode');
+  document
+    .getElementsByClassName('tui-page-btn tui-is-disabled tui-prev')[0]
+    .classList.toggle('night-mode');
 }
