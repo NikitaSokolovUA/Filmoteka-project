530d5ea4a9b7bf9bd903a8e54b91c9f8ef35b6eb
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-10-01 02:31:17 +0300

    Adds features

--- src/js/day_night.js
+++ src/js/day_night.js
@@ -4,9 +4,15 @@ function onChangeTheme() {
   document.body.classList.toggle('night-mode');
   document.getElementsByClassName('footer')[0].classList.toggle('night-mode');
   document
-    .getElementsByClassName('my-pagination')[0]
+    .getElementsByClassName('tui-page-btn tui-prev')[0]
     .classList.toggle('night-mode');
   document
-    .getElementsByClassName('tui-page-btn tui-is-disabled tui-prev')[0]
+    .getElementsByClassName('tui-page-btn tui-next')[0]
+    .classList.toggle('night-mode');
+  for (el of document.getElementsByClassName('film__title-main')) {
+    el.classList.toggle('night-mode');
+  }
+  document
+    .getElementsByClassName('backdrop__modal')[0]
     .classList.toggle('night-mode');
 }
