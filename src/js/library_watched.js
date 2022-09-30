1f732ca10768a6f1cffdd01df6ac0a5f52b2ca35
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-09-30 14:02:17 +0300

    Pagination fix

--- src/js/library_watched.js
+++ src/js/library_watched.js
@@ -1,12 +1,14 @@
 import renderFilmCard from './renderCard';
-import {watchedKeyStorage} from './watched-list';
+import { watchedKeyStorage } from './watched-list';
 import FilmsPagination from './pagination';
 
 function loadWatchedFilms() {
   const watchedFilms = JSON.parse(localStorage.getItem(watchedKeyStorage));
-  renderFilmCard(watchedFilms.slice(0, 20));
-  const paginator = new FilmsPagination(null, watchedFilms.length);
-  paginator.pagination.on('afterMove', paginatePage);
+  if (watchedFilms) {
+    renderFilmCard(watchedFilms.slice(0, 20));
+    const paginator = new FilmsPagination(null, watchedFilms.length);
+    paginator.pagination.on('afterMove', paginatePage);
+  }
 }
 loadWatchedFilms();
 
@@ -15,6 +17,9 @@ function paginatePage(event) {
   const watchedFilms = JSON.parse(localStorage.getItem(watchedKeyStorage));
   const films = document.querySelector('.film__list');
   films.innerHTML = '';
-  films_array = watchedFilms.slice(currentPage * 20 - 20, currentPage * 20);
+  const films_array = watchedFilms.slice(
+    currentPage * 20 - 20,
+    currentPage * 20
+  );
   renderFilmCard(films_array);
 }
