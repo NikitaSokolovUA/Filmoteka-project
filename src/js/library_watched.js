a416cfb3f2a2403e43414c1182f61ae7dbe54680
RomanFalin <romanfalin01@gmail.com>
2022-10-01 11:51:37 +0200

    updated library watched/queue btns

--- src/js/library_watched.js
+++ src/js/library_watched.js
@@ -1,16 +1,27 @@
 import renderFilmCard from './renderCard';
 import { watchedKeyStorage } from './watched-list';
 import FilmsPagination from './pagination';
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'center-top',
+  distance: '10px',
+  opacity: 1,
+});
 
 const watchedListBtn = document.querySelector('.watched-btn-js');
+const filmList = document.querySelector('.film__list');
 
 function loadWatchedFilms() {
   watchedListBtn.classList.add('active-js');
+  filmList.innerHTML = '';
   const watchedFilms = JSON.parse(localStorage.getItem(watchedKeyStorage));
   if (watchedFilms) {
     renderFilmCard(watchedFilms.slice(0, 20));
     const paginator = new FilmsPagination(null, watchedFilms.length);
     paginator.pagination.on('afterMove', paginatePage);
+  } else {
+    Notiflix.Notify.failure('No films in your watched list!');
   }
 }
 
