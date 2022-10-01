3b75c75a2789bfa5c6f4eac18aaaa8ed9701b3b5
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-10-01 14:48:30 +0300

    Squashed commit of the following:
    commit 6a75c3a70aca6f2b14da81f880021f22cd9d828f
    Merge: ac7eb91 bab2732
    Author: NikitaSokolovUA <107192011+NikitaSokolovUA@users.noreply.github.com>
    Date:   Fri Sep 30 22:41:28 2022 +0300
    
        Merge pull request #33 from NikitaSokolovUA/style-renderCard
    
        Style render card
    
    commit bab2732390942e92d297ab7e9c477b794891eb2f
    Author: Nikita Sokolov UA <nsokolov0309@gmail.com>
    Date:   Fri Sep 30 22:41:07 2022 +0300
    
        add fix
    
    commit 991f3d3dd60df69c5ec5b97ceb402c8314a216c5
    Merge: 71d9452 ac7eb91
    Author: Oleksandr Veremchuk <verem4uk@hotmail.com>
    Date:   Fri Sep 30 22:25:57 2022 +0300
    
        Merge branch 'main' into style-renderCard
    
    commit 71d94521b2a58f62c4592907ac99d1c91cfab184
    Author: Oleksandr Veremchuk <verem4uk@hotmail.com>
    Date:   Fri Sep 30 22:12:02 2022 +0300
    
        add styles

--- src/js/renderCard.js
+++ src/js/renderCard.js
@@ -28,16 +28,18 @@ export default async function renderFilmCard(films) {
 
       let poster = '';
       if (poster_path === null) {
-        poster = `src = "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg" width="100%" height="100%"`;
+        poster = `src = "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"`;
       } else poster = `src ="${BASE_URL}${poster_path}"`;
       //  верстка готової карточки фільма
       return `
         <li class="film__card" id=${id}>
-            <a class="film__poster"><img class="film__image" ${poster} alt="${title}" loading="lazy" /></a>
-            <div class="film__info">
-                <p class="film__title">${addAudit(title)}</p>
-                <p class="film__ganre">${genre} | ${addAudit(date)}</p>
-            </div>
+          <div class="film__poster">
+            <img class="film__image" ${poster} alt="${title}" loading="lazy" />
+          </div>
+          <div class="film__info">
+            <p class="film__title-main">${addAudit(title)}</p>
+            <p class="film__ganre">${genre} | ${addAudit(date)}</p>
+          </div>
         </li>
     `;
     })
