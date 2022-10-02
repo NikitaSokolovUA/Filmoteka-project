07d4cc04a79bc1e1bde3c4ddb14731f00f5286fe
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-10-02 23:02:29 +0300

    Squashed commit of the following:
    commit 239cef46001c19a1753ba34fdf7cc671237c6adf
    Author: Nikita Sokolov UA <nsokolov0309@gmail.com>
    Date:   Sun Oct 2 21:48:18 2022 +0300
    
        Update _common.scss
    
    commit e667fd9048ac2447276a9de69b885194a7a6defc
    Author: Nikita Sokolov UA <nsokolov0309@gmail.com>
    Date:   Sun Oct 2 21:39:59 2022 +0300
    
        fix team modal for esc
    
    commit 04a084b593a5f97d7670baba2661b1cca66d6f6f
    Author: Nikita Sokolov UA <nsokolov0309@gmail.com>
    Date:   Sun Oct 2 21:17:04 2022 +0300
    
        Update modal.js
    
    commit 828514a7d4e95ea0c4f7a14e73990e24065a2497
    Merge: 8467646 aa395a1
    Author: NikitaSokolovUA <107192011+NikitaSokolovUA@users.noreply.github.com>
    Date:   Sun Oct 2 21:10:20 2022 +0300
    
        Merge pull request #55 from NikitaSokolovUA/bugfix
    
        sign is fixed
    
    commit aa395a12a4c33ff3a9dc9f78a310f4dc06b03d25
    Author: Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
    Date:   Sun Oct 2 17:48:49 2022 +0300
    
        sign is fixed

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -6,11 +6,36 @@
   };
 
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  // refs.closeModalBtn.addEventListener('click', toggleModal);
-  console.log();
+  refs.openModalBtn.addEventListener('click', onOpenTeamModal);
+  refs.closeModalBtn.addEventListener('click', onCloseTeamModal);
+  refs.modal.addEventListener('click', onClickBackdropModalClose)
 
-  function toggleModal() {
+
+ 
+
+  function onOpenTeamModal() {
+    window.addEventListener('keydown', inKeyDownEscModalClose);
     refs.modal.classList.toggle('is-hidden');
   }
+
+  function onCloseTeamModal() {
+    window.removeEventListener('keydown', inKeyDownEscModalClose);
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  function inKeyDownEscModalClose(event) {
+    const KEY_CODE_ESCAPE = 'Escape';
+
+    if (event.code === KEY_CODE_ESCAPE) {
+      onCloseTeamModal();
+    }
+  } 
+
+  function onClickBackdropModalClose(event) {
+  if (event.target === event.currentTarget) {
+    onCloseTeamModal();
+  }
+}
+
+
 })();
